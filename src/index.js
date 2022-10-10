7ead98f6e8bcb1a38c709ecef81e62e29117f3d6
Tiago Pavani <tiagopavanisalto@hotmail.com>
2022-10-10 17:03:55 -0300

    add api

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-const express = require('express')
-const app = express();
 
-const api = () =>{
-    fetch('https://api.github.com/users/PavaniTiago');
-    then(response => response.json())
+const url = 'https://api.github.com/users/PavaniTiago';
+
+async function getUsers(){
+    await fetch(url)
+    .then(res =>{
+        return res.json()
+    }) 
+    .then(response => {
+        document.getElementById('userName').innerHTML = response.login
+        document.getElementById('repos').innerHTML = response.public_repos
+        document.getElementById('followers').innerHTML = response.followers
+
+    })
+    .catch(e => console.log(e))
 }
 
-app.listen('8000')
-
+getUsers()
