406e8be740b91017bb819ecd97e3d4a8f549a096
Tiago Pavani <tiagopavanisalto@hotmail.com>
2022-11-01 14:16:38 -0300

    add user pfp

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const mainInput = document.getElementById('nome');
 const userName = document.getElementById('userName');
 const userRepos = document.getElementById('repos');
-const userFollowers =  document.getElementById('followers');
+const userFollowers = document.getElementById('followers');
+const pfp = document.getElementById('userPfp');
 
 const form = document.querySelector('.form')
 
@@ -16,17 +17,20 @@ async function getUsers(user){
 }
 
 const renderContent = async(user) =>{
+    userName.innerHTML = 'loading...'
     const data = await getUsers(user)
 
     if(data){
         userName.innerHTML = data.login
         userRepos.innerHTML = data.public_repos
         userFollowers.innerHTML = data.followers
+        pfp.src = data.avatar_url
         mainInput.value = ''
     }else{
-        userName.innerHTML = ''
+        userName.innerHTML = 'Not found :('
         userRepos.innerHTML = ''
         userFollowers.innerHTML = ''
+        pfp.style.display = 'none';
     }
 
     form.addEventListener('submit', (event) =>{
@@ -36,4 +40,3 @@ const renderContent = async(user) =>{
 }
 
 renderContent(getUsers)
-
